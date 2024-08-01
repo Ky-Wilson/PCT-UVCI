fa7e52da2afd4544b08bb6851e1027645e8e708b
Ky-Wilson <yanne.kouassi@epitech.eu>
2024-08-01 12:12:17 +0000

    [implement p1] implementation of home, login and register form

---
+++ UVCI_PCT/routes/web.php
@@ -0,0 +1,38 @@
+<?php
+
+
+use App\Http\Livewire\Admin\AdminDashboardComponent;
+use App\Http\Livewire\Customer\CustomerDashboardComponent;
+use App\Http\Livewire\HomeComponent;
+use App\Http\Livewire\Sprovider\SproviderDashboardComponent;
+use Illuminate\Support\Facades\Route;
+
+
+
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/',HomeComponent::class)->name('home');
+
+// for customer
+Route::middleware(['auth:sanctum', 'verified'])->group(function(){
+    Route::get('/customer/dashboard', CustomerDashboardComponent::class)->name('customer.dashboard');
+});
+// for Sprovider
+Route::middleware(['auth:sanctum', 'verified', 'authsprovider'])->group(function(){
+    Route::get('/sprovider/dashboard', SproviderDashboardComponent::class)->name('sprovider.dashboard');
+});
+// for Admin
+Route::middleware(['auth:sanctum', 'verified', 'authadmin'])->group(function(){
+    Route::get('/admin/dashboard', AdminDashboardComponent::class)->name('admin.dashboard');
+});
